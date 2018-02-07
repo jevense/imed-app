080c49c301fef0de9108c0f57287b9ffdfe2bc3d
lujiewen <jevense@qq.com>
2018-02-07 16:08:50 +0800

    init

--- app/Root.js
+++ app/Root.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import configureStore from './store/ConfigureStore';
+import {Platform} from 'react-native';
 
-import App from './container/App';// app的入口
+
+import App from './container/App';
+// app的入口
 const store = configureStore();
+
+
+const prefix = Platform.select({
+    ios: 'imed://',
+    android: 'imed://imed/'
+})
+
+
 export default class Root extends Component {
     render() {
         return (
             <Provider store={store}>
-                <App/>
+                <App uriPrefix={prefix}/>
             </Provider>
         )
     }
